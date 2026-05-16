48d42a41274acc5258515c4d791682bc40d952a1
TAREKZAINALDIN <tarekzindin@gmail.com>
2026-05-16 20:09:09 +0200

    install lib and rm venv again

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import logging 
 from fastapi  import FastAPI
 from dotenv import load_dotenv
-import os   
+import inngest 
+import inngest.fast_api
+from dotenv import load_dotenv
+import uuid 
+import os 
+import datetime
 
 app = FastAPI()
